2e66c738241c07b5dc6cd96c569cdcea27cffdd1
jenilprince <jenilprince2007@gmail.com>
2025-07-07 10:56:12 +0530

    first

--- practice/p10.c
+++ practice/p10.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 int main(){
-    int num = 27219,check,last;
+    int num = 246802,check=0,last;
     for (int i=num;i>0;i/=10){
-        last=num%10;
-        if (last!=2||last!=4||last!=6||last!=8||last==0){
+        last=i%10;
+        if (last==2||last==4||last==6||last==8||last==0){
+            check=1;
+        }else{
+            check=0;
+            break;
+        }}
+        if (check==1){
+            printf("Yes");
+        }
+        else{
             printf("No");
         }
-    }
+
 }
