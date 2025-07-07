dcb66b23bb7cacbb9f789faf994347df02842ba7
jenilprince <jenilprince2007@gmail.com>
2025-07-07 12:10:38 +0530

    updated p13.c

--- practice/p13.c
+++ practice/p13.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-void max(int arr[5]){
+void max(int arr[10]){
     int b=arr[0];
-    for (int i=0;i<5;i++){
+    for (int i=0;i<10;i++){
         if (arr[i]>b){
             b=arr[i];
         }
     }printf("Max value: %d",b);
 }
 int main(){
-    int arr[5]={1,2,3,4,8};
+    int arr[10]={1,9,3,4,8,10,23,453,34,33};
     max(arr);
 }
-
-
